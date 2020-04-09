c06238a29b877b10bdd90d7ceecb37b1ede1afc6
surelis segarra <sdarianne34@gmail.com>
2020-04-09 10:31:34 -0700

    download sidebar library

---
+++ twitter-bot-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css';
+import Sidebar from '../src/components/Sidebar'
+// import TweetList from './components/TweetList';
+
+
+export default class App extends Component {
+  
+  // state = {fakeTweets: []}
+
+  render() {
+    // const { fakeTweets } = this.state;
+    return (
+            <div>
+              <Sidebar/>
+              {/* <TweetList/> */}
+            </div>
+    )
+  }
+}
---
+++ twitter-bot-frontend/src/components/TweetItem.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react'
+
+// export default class TweetItem extends Component {
+   
+//     render() {
+//        const tweets = this.props.tweet
+//         return (
+//          <div className='TheTweet'>
+            
+//         <li className='tweets' key={tweets._id}>{tweets.tweet}</li>
+             
+//          </div>
+//         )
+//     }
+// }
