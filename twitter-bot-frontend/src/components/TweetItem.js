32d10b378116e070a16f96c770fa1e3e2e60ceaf
Josh Ford <thisisjoshford@gmail.com>
2020-04-10 01:07:27 -0700

    cs styling

--- twitter-bot-frontend/src/components/TweetItem.js
+++ twitter-bot-frontend/src/components/TweetItem.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
-
+import './TweetItem.css';
 export default class TweetItem extends Component {
    
     render() {
        const { approvedTweet } = this.props;
        
         return (
-         <div className='TheTweet'>
-            
+         <div className='tweetBox'>
             <li className='tweets'> { approvedTweet }</li>
-             
          </div>
         )
     }
